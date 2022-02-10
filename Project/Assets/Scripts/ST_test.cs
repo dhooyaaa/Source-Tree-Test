72d399886f4d8f0d1db5eebbf017ae9ede21a019
HyunDongHo <46379443+HyunDongHo@users.noreply.github.com>
2022-02-10 21:40:47 +0900

    ST_test.cs
    함수 test 구현부 작성

---
+++ Project/Assets/Scripts/ST_test.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ST_test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void test()
+    {
+        Debug.Log("ssss");
+    }
+}
