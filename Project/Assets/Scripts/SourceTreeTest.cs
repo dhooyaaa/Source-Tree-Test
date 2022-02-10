9c5523c75fc314614885f507a5e84e0c943141b5
HyunDongHo <46379443+HyunDongHo@users.noreply.github.com>
2022-02-10 19:17:45 +0900

    SourceTreeTest.cs
    어떤 기능 구형

---
+++ Project/Assets/Scripts/SourceTreeTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceTreeTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int price = 1;  
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
