41dfbee9202e6f836bb480856116a930c32a5421
HyunDongHo <46379443+HyunDongHo@users.noreply.github.com>
2022-02-10 20:04:23 +0900

    sourcetree 변수 선언 및 test함수 구현부 수정

--- Project/Assets/Scripts/SourceTreeTest.cs
+++ Project/Assets/Scripts/SourceTreeTest.cs
@@ -7,7 +7,7 @@ public class SourceTreeTest : MonoBehaviour
     // Start is called before the first frame update
 
     public bool completeDone = false;
-
+    public int sourcetree = 0;
     void Start()
     {
         
@@ -23,5 +23,7 @@ public class SourceTreeTest : MonoBehaviour
     {
         int c = 1;
         Debug.Log($"c : {c}");
+        Debug.Log($"sourcetree : {sourcetree}");
+
     }
 }
