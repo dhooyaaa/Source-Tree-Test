f7506ad6a96bcf9beb91cd2da518dbbf54e70708
HyunDongHo <46379443+HyunDongHo@users.noreply.github.com>
2022-02-10 20:35:29 +0900

    ttt 변수 선언 및 test 함수 구현부 수정

--- Project/Assets/Scripts/SourceTreeTest.cs
+++ Project/Assets/Scripts/SourceTreeTest.cs
@@ -9,7 +9,7 @@ public class SourceTreeTest : MonoBehaviour
     public bool completeDone = false;
     public int sourcetree = 0;
     public float kkk ;
-    public int test = 1;
+    public int ttt = 1;
     void Start()
     {
         
@@ -24,7 +24,7 @@ public class SourceTreeTest : MonoBehaviour
     void test()
     {
         int new_d = 1;
-        Debug.Log($"test : {test}");
+        Debug.Log($"ttt : {ttt}");
         Debug.Log($"sourcetree : {sourcetree}");
 
     }
